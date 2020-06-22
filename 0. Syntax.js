738f1e40c4c3feb85de33994be3d99bb60bee2b9
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-22 07:43:37 +0700

    Update 0. Syntax.js

--- 0. Syntax.js
+++ 0. Syntax.js
@@ -1,3 +1,8 @@
 const firstName = "Alexander";
 const lastName = "Julius";
 const arr = ["Teaching", 23, True, function() {console.log('Hi')}];
+
+// Comment
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
