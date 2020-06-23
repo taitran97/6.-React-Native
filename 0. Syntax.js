51313237210440348093f4c8507101dc0d89b23b
Alexander <taitdgbs15312@fpt.edu.vn>
2020-06-23 07:21:34 +0700

    Delete 0. Syntax.js

--- 0. Syntax.js
+++
@@ -1,8 +0,0 @@
-const firstName = "Alexander";
-const lastName = "Julius";
-const arr = ["Teaching", 23, True, function() {console.log('Hi')}];
-
-// Comment
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}
